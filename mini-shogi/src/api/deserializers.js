dcca130b8d47c389a6d4ff5c5f8d8749502f205a
Kedar Rudrawar <rudrawarkedar@gmail.com>
2020-05-09 12:09:33 -0700

    created config for const vars

--- mini-shogi/src/api/deserializers.js
+++ mini-shogi/src/api/deserializers.js
@@ -1,3 +1,17 @@
+import {BOARDSIZE} from "../config";
+
+let initializeBoard = (size) => {
+    let board = [];
+    for(let i = 0; i < size; i++){
+        let row = [];
+        for(let j = 0; j < size; j++){
+            row.push('');
+        }
+        board.push(row);
+    }
+    return board;
+};
+
 let getLocation = (piece) => {
     let row = 4 - piece['location']['row'];
     let col = piece['location']['col'];
@@ -5,10 +19,7 @@ let getLocation = (piece) => {
 };
 
 export function board_to_array(boardObj){
-    let boardToReturn = [];
-    for(let i = 0; i < 5; i++){
-        boardToReturn.push(['','','','','']);
-    }
+    let boardToReturn = initializeBoard(BOARDSIZE);
     let boardArray = boardObj['boardArray'];
     for(let i = 0; i < boardArray.length; i++) {
         for (let j = 0; j < boardArray[i].length; j++) {
